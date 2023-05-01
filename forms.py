73b568d2ac29395971c8bfae52c0e212e7851509
Adrian Szalecki <szalecki.adrian@gmail.com>
2023-05-01 10:54:51 +0200

    trying GitHub Pages

--- forms.py
+++ forms.py
@@ -11,12 +11,6 @@ positions = [(None, ''), ('Toplane', 'Toplane'), ('Jungle', 'Jungle'),
              ('Midlane', 'Midlane'), ('Bottom', 'Bottom'), ('Support', 'Support')]
 divisions = [(None, ''), ('Iron', 'Iron'), ('Bronze', 'Bronze'), ('Silver', 'Silver'), ('Gold', 'Gold'), ('Platinum', 'Platinum'),
              ('Diamond', 'Diamond'), ('Master', 'Master'), ('Grandmaster', 'Grandmaster'), ('Challenger', 'Challenger')]
-months = [(1, 'January'), (2, 'February'), (3, 'March'), (4, 'April'), (5, 'May'), (6, 'June'),
-          (7, 'July'), (8, 'August'), (9, 'September'), (10, 'October'), (11, 'November'), (12, 'December')]
-months_days = {'January': 31, 'February': 28, 'March': 31, 'April': 30, 'May': 31, 'June': 30,
-               'July': 31, 'August': 31, 'September': 30, 'October': 31, 'November': 30, 'December': 31}
-month_dict = {month_name: month_num for month_num, month_name in months}
-month_dict2 = {month_num: month_name for month_num, month_name in months}
 
 
 def get_server_short_name(server_name):
